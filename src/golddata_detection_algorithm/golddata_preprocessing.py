0534918ccb9abc2fb4dba9618a747ffb9339e09d
HuGanghui <huganghui18@gmail.com>
2021-03-16 16:14:51 +0800

    根据sr、rr小于100或者响应时间超过3-Sigma来判断黄金指标是否异常

---
+++ src/golddata_detection_algorithm/golddata_preprocessing.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @File    :   golddata_preprocessing.py    
+# @Contact :   19120364@bjtu.edu.com
+
+# @Modify Time      @Author    @Version    @Description
+# ------------      -------    --------    -----------
+# 2021/3/15 9:08 PM   hgh      1.0         None
+import pickle
+
+import pandas as pd
+
+from utils import get_low_limit, get_upper_limit
+
+
+class GOLDDataPreProcessing:
+    """
+    获取黄金指标不同tc的均值和上下基线
+    """
+    def __init__(self, data_path, sigma=3):
+        """
+        :param data_path: 文件路径，代表用哪一天的数据来获取该系统的均值和上下界限
+        :param sigma:
+        """
+        self.data_path = data_path
+        self.sigma = sigma
+        self.id2mean = dict()
+        self.id2std = dict()
+        self.id2baseline = dict()
+
+    def get_data_from_file(self):
+        self.dataFrame = pd.read_csv(self.data_path, engine='python')
+        self.get_dataframe_list()
+
+    def get_dataframe_list(self):
+        group = self.dataFrame.groupby("tc")
+        keys = self.dataFrame["tc"].unique()
+        dataframes = list(group)
+        for ele in dataframes:
+            tc = ele[1]["tc"].unique()[0]
+            print(tc)
+            self.id2mean[tc] = ele[1]["mrt"].mean()
+            self.id2std[tc] = ele[1]["mrt"].std()
+
+    def get_id_upper_and_lower(self):
+        for id in self.id2mean.keys():
+            self.id2baseline[id] = [get_low_limit(self.sigma, self.id2mean[id], self.id2std[id]),
+                                    get_upper_limit(self.sigma, self.id2mean[id], self.id2std[id])]
+            # print(id + str(self.id2baseline[id]))
+
+    def dumps(self):
+        baseline_path = "./" + self.data_path.split("/")[-3] + "_" \
+                        + self.data_path.split("/")[-1] + "_golddata_baseline.txt"
+        with open(baseline_path, "wb") as f:
+            pickle.dump(self.id2baseline, f, 1)
+
+
+if __name__ == '__main__':
+    system = "b"
+    if system == "a":
+        # system-a
+        data_path = "../../data/system-a/kpi/kpi_0301.csv"
+        test = GOLDDataPreProcessing(data_path, sigma=3)
+        test.get_data_from_file()
+        test.get_id_upper_and_lower()
+        test.dumps()
+        with open("./system-a_kpi_0301.csv_golddata_baseline.txt", "rb") as f:
+            a = pickle.load(f)
+        print(a)
+    else:
+        # system-b
+        data_path = "../../data/system-b/kpi/kpi_0130.csv"
+        test = GOLDDataPreProcessing(data_path, sigma=3)
+        test.get_data_from_file()
+        test.get_id_upper_and_lower()
+        test.dumps()
+        with open("./system-b_kpi_0130.csv_golddata_baseline.txt", "rb") as f:
+            a = pickle.load(f)
+        print(a)
+
--- src/golddata_detection_algorithm/simple_threadhold_detection.py
+++ src/golddata_detection_algorithm/simple_threadhold_detection.py
@@ -7,16 +7,24 @@
 # @Modify Time      @Author    @Version    @Description
 # ------------      -------    --------    -----------
 # 2021/3/13 12:07 PM   hgh      1.0         None
+import pickle
+import time
 
 
 class SimpleThreadHoldDetection:
-    def __init__(self):
+    """
+    根据sr、rr小于100或者响应时间超过3-Sigma来判断黄金指标是否异常
+    """
+    def __init__(self, baseline_path):
         self.stream_kpi = None
+        with open(baseline_path, "rb") as f:
+            self.id2baseline = pickle.load(f)
 
-    def detect(self, stream_kpi, mrt_threshold):
+    def detect(self, stream_kpi):
         self.stream_kpi = stream_kpi
-        if self.rr_smaller_100() or self.sr_smaller_100() \
-                or self.mrt_detect(mrt_threshold):
+        # if self.rr_smaller_100() or self.sr_smaller_100() \
+        #         or self.mrt_detect():
+        if self.mrt_detect():
             return self.stream_kpi["timestamp"]
         else:
             return None
@@ -27,13 +35,47 @@ class SimpleThreadHoldDetection:
     def sr_smaller_100(self):
         return self.stream_kpi["sr"] < 100
 
-    def mrt_detect(self, mrt_threshold):
-        return self.stream_kpi["mrt"] > mrt_threshold
+    def mrt_detect(self):
+        result = self.stream_kpi["mrt"] > self.id2baseline[self.stream_kpi["tc"]][1] \
+                 or self.stream_kpi["mrt"] < self.id2baseline[self.stream_kpi["tc"]][0]
+        if result:
+            pass
+            # print(self.stream_kpi["tc"])
+        return result
+
+
+def gold_test(test_data_path, baseline_path, system):
+    i = 0
+    simplemodel = SimpleThreadHoldDetection(baseline_path)
+    with open(test_data_path, "r") as f:
+        count = 0
+        prev_time = None
+        for line in f:
+            if i > 0:
+                lines = line.split(",")
+                msg = dict()
+                msg["timestamp"] = int(lines[0])
+                msg["rr"] = float(lines[1])
+                msg["sr"] = float(lines[2])
+                if system == "a":
+                    msg["count"] = float(lines[3])
+                else:
+                    msg["cnt"] = float(lines[3])
+                msg["mrt"] = float(lines[4])
+                msg["tc"] = lines[5].strip()
+                result = simplemodel.detect(msg)
+                if result is not None:
+                    strtime = time.strftime("%Y--%m--%d %H:%M:%S", time.localtime(result))
+                    if strtime != prev_time:
+                        print(strtime)
+                        count += 1
+                    prev_time = strtime
+            else:
+                i += 1
+        print(count)
 
 
 if __name__ == '__main__':
-    msg = {"timestamp": 1614268800, "rr": 100, "sr": 100,
-           "count": 106, "mrt": 11, "tc": "aaa51527e6254b04dd1d25004e6da3e6db651e4e"}
-    test = SimpleThreadHoldDetection()
-    result = test.detect(msg, 100)
-    print(result)
+    gold_test("../../data/system-a/kpi/kpi_0226.csv",
+         "./system-a_kpi_0301.csv_golddata_baseline.txt",
+         "a")
